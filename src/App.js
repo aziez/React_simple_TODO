5735003ed5058ce11c19af2cdf798d618e64425e
Aziez <Aziz13.id@gmail.com>
2022-10-31 18:52:23 +0700

    First

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import List  from './component/List';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+        todoItem: '',
+        todoList: []
+    }
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+      todoList: [...this.state.todoList, this.state.todoItem],
+      todoItem: ''
+    });
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      todoItem: event.target.value
+    });
+  }
+  
+
+  render(){
+    return(
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" placeholder="Enter your name" value={this.state.todoItem} onChange={this.handleChange} />
+          <button>Submit</button>
+        </form>
+        <List todoList={this.state.todoList} />
+      </div>
+    )
+  }
 }
 
 export default App;
